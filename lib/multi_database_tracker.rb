ce51da73842487794a13e21cbb60ec93a7ec938c
Cassie Achzenick <caachzenick@gmail.com>
2020-01-07 17:58:35 -0700

    Add attempt at inheritance

---
+++ lib/multi_database_tracker.rb
@@ -0,0 +1,51 @@
+require_relative 'game_team'
+require_relative 'game'
+require_relative 'team'
+
+class MultiDatabaseTracker
+  attr_reader :game_path, :team_path, :game_teams_path, :game_teams, :games, :teams
+
+  def self.from_csv(locations)
+    game_path = locations[:games]
+    team_path = locations[:teams]
+    game_teams_path = locations[:game_teams]
+    MultiDatabaseTracker.new(game_path, team_path, game_teams_path)
+  end
+
+  def initialize(game_path, team_path, game_teams_path)
+    @game_teams = GameTeam.from_csv(game_teams_path)
+    @games = Game.from_csv(game_path)
+    @teams = Team.from_csv(team_path)
+  end
+
+  def worst_fans
+    unique_teams = @game_teams.reduce({}) do |acc, game_team|
+      acc[game_team.team_id] = {away: 0, home: 0}
+      acc
+    end
+
+    @game_teams.each do |game_team|
+      if game_team.hoa == "away" && game_team.result == "WIN"
+        unique_teams[game_team.team_id][:away] += 1
+      elsif game_team.hoa == "home" && game_team.result == "WIN"
+        unique_teams[game_team.team_id][:home] += 1
+      end
+    end
+
+    worst_fans_are = unique_teams.find_all do |key, value|
+      value[:away] > value[:home]
+    end.to_h
+
+    worst_teams = worst_fans_are.to_h.keys
+
+    final = worst_teams.map do |team2|
+      @teams.find do |team1|
+        team2 == team1.team_id
+      end
+    end
+
+    finnalist = final.map do |team|
+      team.teamname
+    end
+  end
+end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,25 +1,33 @@
-require_relative 'game_team'
-require_relative 'game'
-require_relative 'team'
+# require_relative 'game_team'
+# require_relative 'game'
+# require_relative 'team'
+require_relative 'multi_database_tracker'
 
-class StatTracker
-  attr_reader :game_path, :team_path, :game_teams_path, :game_teams, :games, :teams
+class StatTracker < MultiDatabaseTracker
 
   def self.from_csv(locations)
-    game_path = locations[:games]
-    team_path = locations[:teams]
-    game_teams_path = locations[:game_teams]
-    StatTracker.new(game_path, team_path, game_teams_path)
+    super
+    @game_teams = super.game_teams
+    @games = super.games
+    @teams = super.teams
   end
-
-  def initialize(game_path, team_path, game_teams_path)
-    @game_path = game_path
-    @team_path = team_path
-    @game_teams_path = game_teams_path
-    @game_teams = GameTeam.from_csv(@game_teams_path)
-    @games = Game.from_csv(@game_path)
-    @teams = Team.from_csv(@team_path)
-  end
+  # attr_reader :game_path, :team_path, :game_teams_path, :game_teams, :games, :teams
+  #
+  # def self.from_csv(locations)
+  #   game_path = locations[:games]
+  #   team_path = locations[:teams]
+  #   game_teams_path = locations[:game_teams]
+  #   StatTracker.new(game_path, team_path, game_teams_path)
+  # end
+  #
+  # def initialize(game_path, team_path, game_teams_path)
+  #   @game_path = game_path
+  #   @team_path = team_path
+  #   @game_teams_path = game_teams_path
+  #   @game_teams = GameTeam.from_csv(@game_teams_path)
+  #   @games = Game.from_csv(@game_path)
+  #   @teams = Team.from_csv(@team_path)
+  # end
 
   def highest_total_score
     Game.highest_total_score
@@ -77,35 +85,39 @@ class StatTracker
     Team.team_info(id)
   end
 
+  # def worst_fans
+  #   unique_teams = @game_teams.reduce({}) do |acc, game_team|
+  #     acc[game_team.team_id] = {away: 0, home: 0}
+  #     acc
+  #   end
+  #
+  #   @game_teams.each do |game_team|
+  #     if game_team.hoa == "away" && game_team.result == "WIN"
+  #       unique_teams[game_team.team_id][:away] += 1
+  #     elsif game_team.hoa == "home" && game_team.result == "WIN"
+  #       unique_teams[game_team.team_id][:home] += 1
+  #     end
+  #   end
+  #
+  #   worst_fans_are = unique_teams.find_all do |key, value|
+  #     value[:away] > value[:home]
+  #   end.to_h
+  #
+  #   worst_teams = worst_fans_are.to_h.keys
+  #
+  #   final = worst_teams.map do |team2|
+  #     @teams.find do |team1|
+  #       team2 == team1.team_id
+  #     end
+  #   end
+  #
+  #   finnalist = final.map do |team|
+  #     team.teamname
+  #   end
+  # end
+
   def worst_fans
-    unique_teams = @game_teams.reduce({}) do |acc, game_team|
-      acc[game_team.team_id] = {away: 0, home: 0}
-      acc
-    end
-
-    @game_teams.each do |game_team|
-      if game_team.hoa == "away" && game_team.result == "WIN"
-        unique_teams[game_team.team_id][:away] += 1
-      elsif game_team.hoa == "home" && game_team.result == "WIN"
-        unique_teams[game_team.team_id][:home] += 1
-      end
-    end
-
-    worst_fans_are = unique_teams.find_all do |key, value|
-      value[:away] > value[:home]
-    end.to_h
-
-    worst_teams = worst_fans_are.to_h.keys
-
-    final = worst_teams.map do |team2|
-      @teams.find do |team1|
-        team2 == team1.team_id
-      end
-    end
-
-    finnalist = final.map do |team|
-      team.teamname
-    end
+    super
   end
 
   def best_fans
